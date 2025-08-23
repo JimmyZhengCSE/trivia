a726b922487dc663aa10b17a8c35ebb71c7b1b13
Jimmy Zheng <nyc.jimmyzheng@gmail.com>
2025-08-22 22:28:22 -0400

    updated ports

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
 import random
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -38,4 +39,5 @@ def quiz():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    port = os.getenv('PORT', 8000)
+    app.run(host='0.0.0.0', port=int(8000))
